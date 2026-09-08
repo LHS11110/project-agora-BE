f3c96a442b0b575df35b334973b50f96d916077e
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2026-09-08 18:19:59 +0900

    refactor: remove entire Frelog module and associated Elasticsearch and Redis infrastructure components

--- src/main/java/com/endpoint/frelog/repository/elasticsearch/DataRecordElasticsearchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.endpoint.frelog.repository.elasticsearch;
-
-import com.endpoint.frelog.domain.elasticsearch.DataRecordDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataRecordElasticsearchRepository extends ElasticsearchRepository<DataRecordDocument, String> {
-}
--- src/main/java/com/endpoint/frelog/repository/jpa/DataRecordJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.endpoint.frelog.repository.jpa;
-
-import com.endpoint.frelog.domain.jpa.DataRecordEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataRecordJpaRepository extends JpaRepository<DataRecordEntity, String> {
-}
